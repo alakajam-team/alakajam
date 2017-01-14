8bb79d0b4ddad967d12bedceded68cf453e3bd6f
Marwane Kalam-Alami <marwane.ka@gmail.com>
2017-01-14 13:34:27 +0100

    hello world

---
+++ README.md
@@ -0,0 +1,15 @@
+# WeJam
+
+## Server setup
+
+Requirement: NodeJS 7.2+
+
+* `npm install`
+* `npm start`
+* Browse to `http://localhost:8000`
+
+## Development setup
+
+* `npm install -g standard nodemon`
+* Make the app restart automatically upon files changes with `npm run-script nodemon`
+* Validate code syntax with `standard`
---
+++ wejam.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express')
+const expressNunjucks = require('express-nunjucks')
+const system = require('./lib/system')
+
+// App setup
+
+const app = express()
+const isDev = app.get('env') === 'development'
+
+system.gracefulShutdown(app)
+
+app.set('views', path.join(__dirname, '/templates'))
+
+expressNunjucks(app, {
+  watch: isDev,
+  noCache: isDev
+})
+
+// App routing
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+// Launch
+
+app.listen(8000)
+console.log('Listening on port 8000.')
