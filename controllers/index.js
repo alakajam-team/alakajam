b4aead57560b118700aa7a6361b559c7a117fb13
Marwane Kalam-Alami <marwane.ka@gmail.com>
2017-05-03 00:30:46 +0200

    better decoupling of controllers/models from the core

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+
+	initRoutes: function (app) { 
+  	require('./main.js').initRoutes(app)
+	  require('./event.js').initRoutes(app)
+	  require('./entry.js').initRoutes(app)
+	}
+
+}
---
+++ models/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = {
+
+	/*
+	 * List models in table creation order
+	 */
+	modelFilenamesUp: function () {
+		return [
+		  'eventModel',
+		  'entryModel'
+	  ]
+	}
+
+}
