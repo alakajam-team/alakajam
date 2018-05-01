a9d31e8034c77f30f8c37e39d5350b068980dec7
Marwane Kalam-Alami <marwane.ka@gmail.com>
2018-05-01 16:56:51 +0200

    Prevent submitting a form twice: better implementation that doesn't alter form data

--- assets/js/common/form-submit.js
+++ assets/js/common/form-submit.js
@@ -3,6 +3,7 @@
 module.exports = function formSubmit () {
   // Prevent submitting a form twice
   $('form').submit(function () {
-    $('input[type=submit]', this).prop('disabled', true)
+    $('input[type=submit]', this).attr('data-disabled', true)
+    this.onsubmit = () => false
   })
 }
