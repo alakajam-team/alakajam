ecdbb38b706a2d02d9e3c04e257e790516e4f10f
Marwane Kalam-Alami <marwane.ka@gmail.com>
2018-07-05 15:09:47 +0200

    Why not handshakes

--- core/enums.js
+++ core/enums.js
@@ -74,6 +74,6 @@ module.exports = {
     UNRANKED: 'unranked'
   },
   LIKES: {
-    'like': { icon_unliked: 'far fa-heart', icon_liked: 'fas fa-heart', title: 'Like' }
+    'like': { icon_unliked: 'far fa-handshake', icon_liked: 'fas fa-handshake', title: 'Like' }
   }
 }
