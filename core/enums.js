16b18409500fd2c551ae5d95784187533f117795
Marwane Kalam-Alami <marwane.ka@gmail.com>
2018-07-05 15:45:07 +0200

    Why not gems!

--- core/enums.js
+++ core/enums.js
@@ -74,6 +74,6 @@ module.exports = {
     UNRANKED: 'unranked'
   },
   LIKES: {
-    'like': { icon_unliked: 'far fa-kiss-beam', icon_liked: 'fas fa-kiss-beam', title: 'Like' }
+    'like': { icon_unliked: 'far fa-gem', icon_liked: 'fas fa-gem', title: 'Like' }
   }
 }
