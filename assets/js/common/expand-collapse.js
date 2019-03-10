e115682c375f9cb5c6063f2e8cef2cc699b770c6
Marwane Kalam-Alami <marwane.ka@gmail.com>
2019-03-10 15:33:00 +0100

    Close #382 Make featured announcement adapt height to the right sidebar

--- assets/js/common/expand-collapse.js
+++ assets/js/common/expand-collapse.js
@@ -2,23 +2,22 @@
 
 module.exports = function expandCollapse () {
   $('.js-expand-bar').each(function () {
-    LimitExpandContent($(this), $(this).parent());
-  });
+    LimitExpandContent($(this), $(this).parent())
+  })
 
-  $('.expandable img').on('load', function() {
-    var content = $(this).parents('.expandable');
+  $('.expandable img').on('load', function () {
+    var content = $(this).parents('.expandable')
     LimitExpandContent($('.js-expand-bar', content), $(content))
-  });
+  })
 }
 
-function LimitExpandContent(expandBar, content) {
-  var maxHeight = parseInt(expandBar.attr('data-max-height'));
-  console.log(maxHeight);
+function LimitExpandContent (expandBar, content) {
+  var maxHeight = parseInt(expandBar.attr('data-max-height'))
   if (content.height() > maxHeight) {
-    content.attr('style', 'max-height: ' + (maxHeight * 0.8) + 'px'); // limit the size while preventing cropping just a single line
-    expandBar.show();
+    content.attr('style', 'max-height: ' + (maxHeight * 0.8) + 'px') // limit the size while preventing cropping just a single line
+    expandBar.show()
     expandBar.click(function () {
-      content.toggleClass('expanded');
-    });
+      content.toggleClass('expanded')
+    })
   }
 }
