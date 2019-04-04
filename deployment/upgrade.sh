9c1f766a226d7cfa6922340468fd97edcfe150d6
Marwane Kalam-Alami <marwane.ka@gmail.com>
2019-04-04 22:19:49 +0200

    Upgrade script: support resetting data (to be used in nightly build)

--- deployment/upgrade.sh
+++ deployment/upgrade.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
+# Usage: upgrade.sh [pm2 app] [--reset]
+
+cd "$(dirname "$0")/.."
 
 PM2_APP_NAME=${1:-alakajam}
-echo "Upgrading $PM2_APP_NAME..."
 
+if [ "${2}" = "--reset" ]; then
+  echo "Resetting $PM2_APP_NAME data..."
+  rm -rf data
+  rm -rf static/uploads
+fi
+
+echo "Upgrading $PM2_APP_NAME..."
 git checkout -- package-lock.json
 git fetch
 git reset --hard origin
 npm install --production
 npm run deployment:build
+
 pm2 restart $PM2_APP_NAME
