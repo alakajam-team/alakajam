ff4565eb18641dc4770f7d278b6df473a4fe326c
Marwane <marwane.ka@gmail.com>
2020-01-05 01:29:19 +0100

    Bootstrap 4: Bulk class migrations

--- client/scripts/site.ts
+++ client/scripts/site.ts
@@ -20,7 +20,6 @@ import showIfNonempty from "./common/show-if-nonempty";
 import sortableTable from "./common/sortable-table";
 import syncSlug from "./common/sync-slug";
 import syncText from "./common/sync-text";
-import tabs from "./common/tabs";
 import tagsSelect from "./common/tags-select";
 import tooltips from "./common/tooltips";
 import userSelect from "./common/user-select";
@@ -64,7 +63,6 @@ $(function domReady() {
   sortableTable();
   syncSlug(".js-sync-slug");
   syncText();
-  tabs({ buttonContainerSelector: ".js-tab-container", buttonSelector: ".js-tab-button", tabSelector: ".js-tab" });
   tagsSelect();
   tooltips(".has-tooltip");
   userSelect();
