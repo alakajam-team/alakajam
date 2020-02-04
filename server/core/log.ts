a187d818c62480c5ff1e5a791a28f40131d652a3
Marwane Kalam-Alami <marwane.ka@gmail.com>
2020-02-04 20:58:34 +0100

    #414 Upgrade to Winston 3

--- server/core/log.ts
+++ server/core/log.ts
@@ -13,11 +13,11 @@
  */
 
 import { sync as findUp } from "find-up";
+import { TransformableInfo } from "logform";
 import * as luxon from "luxon";
 import * as path from "path";
 import * as util from "util";
-import { createLogger, format, transports } from "winston";
-import winston = require("winston");
+import { createLogger, format, Logger, transports } from "winston";
 
 const SOURCES_ROOT = path.dirname(findUp("package.json", { cwd: __dirname }));
 const LOCAL_TIME_ZONE = new luxon.LocalZone();
@@ -36,46 +36,42 @@ const colorizer = format.colorize();
 /*
  * Custom formatter allows printing pretty, colorful & informative log lines
  */
-const customFormat = {
-  transform: (options) => {
-    // Figure out the logging caller location
-    // XXX slow and hacky approach
-    let location = "?";
-    const lines = new Error().stack.split("\n");
-    for (const line of lines) {
-      if (line.indexOf(SOURCES_ROOT) !== -1 &&
-        line.indexOf(__filename) === -1 &&
-        line.indexOf("node_modules") === -1) {
-        const locInfo = line.replace(/(.*\()/g, "")
-          .replace(process.cwd(), "")
-          .split(/[ :]/g);
-        location = locInfo[locInfo.length - 3].replace("\\", "") +
-          ":" + locInfo[locInfo.length - 2];
-        break;
-      }
+const templateFunction = (options: TransformableInfo): string => {
+  // Figure out the logging caller location
+  // XXX slow and hacky approach
+  let location = "?";
+  const lines = new Error().stack.split("\n");
+  for (const line of lines) {
+    if (line.indexOf(SOURCES_ROOT) !== -1 &&
+      line.indexOf(__filename) === -1 &&
+      line.indexOf("node_modules") === -1) {
+      const locInfo = line.replace(/(.*\()/g, "")
+        .replace(process.cwd(), "")
+        .split(/[ :]/g);
+      location = locInfo[locInfo.length - 3].replace("\\", "") +
+        ":" + locInfo[locInfo.length - 2];
+      break;
     }
-
-    // Build the logging line
-    const timestamp = luxon.DateTime.local().setZone(LOCAL_TIME_ZONE).toFormat("yyyy-MM-dd HH:mm:ss.SSS");
-    const prefix = timestamp + " " + options.level.toUpperCase() + " (" + location + ")";
-    const message = options.message ? (" " + util.format(options.message)) : "";
-    const prefixedMessage = colorizer.colorize(options.level, prefix) + message;
-
-    return {
-      level: options.level,
-      message: prefixedMessage
-    };
   }
+
+  // Build the logging line
+  const timestamp = luxon.DateTime.local().setZone(LOCAL_TIME_ZONE).toFormat("yyyy-MM-dd HH:mm:ss.SSS");
+  const prefix = timestamp + " " + options.level.toUpperCase() + " (" + location + ")";
+  const message = options.message ? (" " + util.format(options.message)) : "";
+  const prefixedMessage = colorizer.colorize(options.level, prefix) + message;
+
+  return prefixedMessage;
 };
 
 const log = createLogger({
   level,
   format: format.combine(
     format.splat(),
-    customFormat
+    format.printf(templateFunction)
   ),
   transports: [new transports.Console()]
 });
+
 
 /**
  * Logs the current stacktrace at info level
@@ -85,6 +81,6 @@ const log = createLogger({
   log.info("I am" + lines.slice(2).join("\n"));
 };
 
-export type Logger = winston.Logger & { whereami: () => void };
+export type CustomLogger = Logger & { whereami: () => void };
 
-export default log as Logger;
+export default log as CustomLogger;
