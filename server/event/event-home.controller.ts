f5711281e2d38ee3a91554484eac695f8f8cd7a1
Marwane Kalam-Alami <marwane.ka@gmail.com>
2020-03-07 21:45:43 +0100

    Fix #507 Display own entry in event home page

--- server/event/event-home.controller.ts
+++ server/event/event-home.controller.ts
@@ -2,18 +2,23 @@ import likeService from "server/post/like/like.service";
 import postService from "server/post/post.service";
 import { CustomRequest, CustomResponse } from "server/types";
 import { EventLocals } from "./event.middleware";
+import eventService from "./event.service";
 
 /**
  * Browse event home page
  */
 export async function viewEventHome(req: CustomRequest, res: CustomResponse<EventLocals>) {
+  const { user, event } = res.locals;
+
   const posts = await postService.findPosts({
-    eventId: res.locals.event.get("id"),
+    eventId: event.get("id"),
     specialPostType: "announcement",
   });
+  const userEntry = user ? await eventService.findUserEntryForEvent(user, event.get("id")) : undefined;
 
   res.render("event/event-home", {
     posts,
+    userEntry,
     userLikes: await likeService.findUserLikeInfo(posts.models, res.locals.user),
   });
 }
