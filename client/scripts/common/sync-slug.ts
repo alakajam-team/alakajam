cd15d105d84a72159f50f5e465a0c950582d1dbc
Marwane Kalam-Alami <marwane.ka@gmail.com>
2020-05-21 00:44:44 +0200

    Fix event slug being autocompleted by browsers + overrided by client script, closes #518

--- client/scripts/common/sync-slug.ts
+++ client/scripts/common/sync-slug.ts
@@ -5,8 +5,10 @@ export default function syncSlug(inputSelector) {
   $(inputSelector)
     .on("change", (event) => {
       const $titleInput = $(event.target);
-      const slugText = slug($titleInput.val()).toLowerCase();
+      const slugText = slug($titleInput.val().toString()).toLowerCase();
       const $slugInput = $($titleInput.attr("data-sync-slug-input-selector"));
-      $slugInput.val(slugText);
+      if (!$slugInput.val()) {
+        $slugInput.val(slugText);
+      }
     });
 }
