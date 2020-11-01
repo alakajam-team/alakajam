c9704204758bdaca052fcdc459194c0c9d837171
Marwane Kalam-Alami <marwane@kalam-alami.net>
2020-11-01 15:39:56 +0100

    Replace uglifyjs webpack plugin with terser, fix production deployment using dev JS build

--- deployment/upgrade.sh
+++ deployment/upgrade.sh
@@ -20,6 +20,6 @@ git checkout -- .
 git fetch
 git reset --hard origin/master
 npm install --production
-npm run build
+NODE_ENV=production npm run build
 
 pm2 restart $PM2_APP_NAME
--- webpack.production.js
+++ webpack.production.js
@@ -1,24 +1,22 @@
-const webpackMerge = require('webpack-merge')
-const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin')
 const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const webpackCommon = require('./webpack.common')
 
 // https://webpack.js.org/guides/production/#setup
 module.exports = webpackMerge(webpackCommon, {
   mode: 'production',
-  // https://webpack.js.org/guides/production/#source-mapping
-  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   plugins: [
     // https://webpack.js.org/guides/production/#specify-the-environment
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production')
       }
-    }),
-    // https://webpack.js.org/guides/tree-shaking/#minify-the-output
-    new UglifyjsWebpackPlugin({
-      sourceMap: true
     }),
     // https://webpack.js.org/guides/production/#module-concatenation
     new webpack.optimize.ModuleConcatenationPlugin()
