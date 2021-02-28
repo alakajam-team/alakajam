7e83dd5616d49b5caa21a73759e4d29e0def5502
Marwane Kalam-Alami <marwane.ka@gmail.com>
2021-02-28 12:52:07 +0100

    Rate limiter test: make tests less time sensitive

--- server/core/rate-limiter.ts
+++ server/core/rate-limiter.ts
@@ -2,6 +2,8 @@ import cache from "./cache";
 
 /**
  * Drops calls to the provided callback in case of too frequent usage.
+ * Note: For protection against attacks, prefer using the route-level limiter (see "sensitiveActionsSlowDown" in routes.ts).
+ * This function is more meant as a fake scheduler do prevent non-critical code from running too frequently.
  * @param key Rate limiting key
  * @param minDelayS Delay during which all calls after the initial one are dropped, in seconds
  * @param callback Function to be called when accepted
