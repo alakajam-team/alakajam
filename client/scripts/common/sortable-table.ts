fb6026368c4b3b1dfd514ebd7d2efada7d922274
Marwane Kalam-Alami <marwane.ka@gmail.com>
2021-02-28 12:42:22 +0100

    Fix sortable tables ignoring numbers, closes #625

--- client/scripts/common/sortable-table.ts
+++ client/scripts/common/sortable-table.ts
@@ -1,5 +1,8 @@
 /* eslint-disable no-unused-expressions */
 
+/**
+ * http://tristen.ca/tablesort/demo/
+ */
 export default function sortable(): void {
   $("table.sortable").each((_index, element) => {
     const tablesortOptions = $(element).data("tablesort-options") || {};
