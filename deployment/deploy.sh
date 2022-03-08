3b11f633700fd7d757b0a7705df21ec2defb13e6
Marwane Kalam-Alami <marwane.ka@gmail.com>
2022-03-08 17:06:28 +0100

    Support customizing deploy.sh build command

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,7 +3,7 @@
 # This temporary script lets you do a build from your machine before deploying it to the server.
 # Prerequisite: add your personal SSH key to Alakajam's authorized_keys
 
-npm run build
+npm run ${BUILD:-build}
 tar -czf dist.tar.gz dist
 scp dist.tar.gz alakajam@alakajam.com:/var/www/alakajam/dist.tar.gz
 
