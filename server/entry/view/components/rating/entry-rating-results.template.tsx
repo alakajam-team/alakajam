502f0c96f5b6ea0d930eccad96d7896f07aa105a
Marwane Kalam-Alami <marwane@kalam-alami.net>
2025-04-21 17:44:53 +0200

    Fix message when entry has not enough ratings

--- server/entry/view/components/rating/entry-rating-results.template.tsx
+++ server/entry/view/components/rating/entry-rating-results.template.tsx
@@ -4,42 +4,37 @@ import React, { JSX } from "preact";
 import { ordinal } from "server/core/formats";
 import links from "server/core/links";
 import { digits } from "server/core/templating-filters";
-import { ifFalse, ifTrue } from "server/macros/jsx-utils";
 import { divisionLabel } from "../division-label";
 
 export function entryRatingResults(entry: BookshelfModel, event: BookshelfModel): JSX.Element {
-  let hasRatings = false;
-  const details = entry.related("details");
   const entriesInDivision = event.related("details").get("division_counts")[entry.get("division")];
+
+  const entryResults = formatEntryResults(event, entry);
+  const hasRankings = entryResults.some(category => category.ranking);
 
   return <div class="entry-results">
     <h2 class="entry-results__header"><a id="results"></a>Voting results</h2>
     <div class="entry-results__body">
       <p>
-        {event.related("details").get("category_titles").map((categoryTitle, index) => {
-          hasRatings = true;
-          const categoryIndex = index + 1;
-          const ranking = details.get("ranking_" + categoryIndex);
-          const rating = details.get("rating_" + categoryIndex);
-
-          if (ranking) {
-            const percentage = (ranking - 1.) / entriesInDivision * 100;
+        {entryResults.map(category => {
+          if (category.ranking) {
+            const percentage = (category.ranking - 1.) / entriesInDivision * 100;
 
             return <div class="entry-results__category">
-              <div class="entry-results__category-title">{categoryTitle}</div>
+              <div class="entry-results__category-title">{category.categoryTitle}</div>
               <div class="entry-results__category-ranking">
-                <a href={`${links.routeUrl(event, "event", "results")}?sortBy=${categoryIndex}&division=${entry.get("division")}`}>
-                  {ifTrue(ranking <= 3, () =>
-                    <span class={`entry-results__category-medal medal-category-${categoryIndex} medal-ranking-${ranking} in-picture`}></span>
-                  )}
-                  {ordinal(ranking)}
+                <a href={`${links.routeUrl(event, "event", "results")}?sortBy=${category.categoryIndex}&division=${entry.get("division")}`}>
+                  {category.ranking <= 3 &&
+                    <span class={`entry-results__category-medal medal-category-${category.categoryIndex} `
+                      + ` medal-ranking-${category.ranking} in-picture`}></span>}
+                  {ordinal(category.ranking)}
                 </a>
               </div>
               <div class="entry-results__category-rating d-none d-sm-inline-block">{percentage > 0 ? digits(percentage, 0) + "%" : ""}</div>
-              <div class="entry-results__category-rating">{digits(rating, 3)}</div>
+              <div class="entry-results__category-rating">{digits(category.rating, 3)}</div>
               <div class="entry-results__category-stars d-none d-sm-inline-block">
                 {range(1, 11).map(i =>
-                  <span class={"fa-lg " + ((i <= rating) ? "fas fa-star" : "far fa-star")}></span>
+                  <span class={"fa-lg " + ((i <= category.rating) ? "fas fa-star" : "far fa-star")}></span>
                 )}
               </div>
             </div>;
@@ -47,15 +42,34 @@ export function entryRatingResults(entry: BookshelfModel, event: BookshelfModel)
         })}
       </p>
 
-      {ifTrue(hasRatings, () =>
-        <div>
-          This game entered in the <strong>{divisionLabel(entry.get("division"))}</strong> competition
-          (<strong>{entriesInDivision}</strong> entries).
-        </div>
-      )}
-      {ifFalse(hasRatings, () =>
-        <div>This entry did not get enough ratings to be ranked.</div>
-      )}
+      <div>
+        This game entered in the <strong>{divisionLabel(entry.get("division"))}</strong> competition
+        (<strong>{entriesInDivision}</strong> entries){!hasRankings && ", but did not get enough ratings to be ranked"}.
+      </div>
     </div>
   </div>;
+}
+
+interface CatagoryResults {
+  categoryIndex: number;
+  categoryTitle: string;
+  rating: number;
+  ranking?: number;
+}
+
+function formatEntryResults(event: BookshelfModel, entry: BookshelfModel): CatagoryResults[] {
+  const entryDetails = entry.related("details");
+
+  return event.related("details").get("category_titles").map((categoryTitle, index) => {
+    const categoryIndex = index + 1;
+    const ranking = entryDetails.get("ranking_" + categoryIndex);
+    const rating = entryDetails.get("rating_" + categoryIndex);
+
+    return {
+      categoryIndex,
+      categoryTitle,
+      ranking,
+      rating
+    };
+  });
 }
